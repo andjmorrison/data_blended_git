7a1f49ccbcfcbd0a266e366d2004bf239c86169a
andjmorrison <andjmorrison@gmail.com>
2022-02-08 19:21:34 -0800

    Add config, H var

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
+# dependencies
+from config import my_var
+
 x = 'Stanley'
 y = 'Cup'
 
-print(f"I don't know what the {x} {y} is all about!")
+my_friend = 'Harrison'
+
+print(f"I don't know what the {x} {y} is all about! However, {my_friend} loves it!")
+print(f'{my_friend} is {my_var - 4} years old!')
