8daea537a53627dd0b0740e07b09da669fd528f6
andjmorrison <andjmorrison@gmail.com>
2022-02-08 19:26:43 -0800

    Add friend dict

--- app.py
+++ app.py
@@ -6,5 +6,19 @@ y = 'Cup'
 
 my_friend = 'Harrison'
 
+# print statement
 print(f"I don't know what the {x} {y} is all about! However, {my_friend} loves it!")
 print(f'{my_friend} is {my_var - 4} years old!')
+
+# friends
+friends_harrison = [{
+    'name':'Popcorn',
+    'type':'fish'
+},
+{
+    'name':'Carlos',
+    'type':'human'
+}
+]
+
+print(friends_harrison.values())
